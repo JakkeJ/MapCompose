c5ea7652fb51b0403fc632108abab9ca1bb8bdfe
JakkeJ <jimtore2k@gmail.com>
2024-03-10 17:26:28 +0100

    Removed demos

--- demo/src/main/java/ovh/plrapps/mapcompose/demo/utils/Random.kt
+++
@@ -1,7 +0,0 @@
-package ovh.plrapps.mapcompose.demo.utils
-
-import kotlin.random.Random.Default.nextDouble
-
-fun randomDouble(center: Double, radius: Double) : Double {
-    return nextDouble(from = center - radius, until = center + radius)
-}
